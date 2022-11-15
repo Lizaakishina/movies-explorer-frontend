dfa606ffef766afadfff0fef43096dbfdeb074a4
LizaAkishina <lizaakishina@gmail.com>
2022-11-15 22:18:38 +0300

    feat: верстка aboutproject (fix: адаптив компонентов)

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import './Main.css';
 import Header from '../Header/Header';
 import Promo from './Promo/Promo';
+import AboutProject from './AboutProject/AboutProject';
 
 const Main = () => {
   return(
     <main className="main__page">
       <Header />
       <Promo />
+      <AboutProject />
     </main>
   );
 }
