64093b48f3bed356b2904af1c8ba322edac04aa8
LizaAkishina <lizaakishina@gmail.com>
2022-11-15 20:24:17 +0300

    feat: верстка header, promo, navtab

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css';
+import Header from '../Header/Header';
+import Promo from './Promo/Promo';
+
+const Main = () => {
+  return(
+    <main className="main__page">
+      <Header />
+      <Promo />
+    </main>
+  );
+}
+
+export default Main;
