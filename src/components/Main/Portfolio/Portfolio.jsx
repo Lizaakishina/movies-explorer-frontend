64ed721d16284f6bab64af8b9960095d850e2630
LizaAkishina <lizaakishina@gmail.com>
2022-11-22 20:36:49 +0300

    feat: разметка компонента portfolio

---
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -0,0 +1,33 @@
+import "./Portfolio.css";
+
+const Portfolio = () => {
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="list portfolio__flexbox">
+        <li>
+          <a href="https://github.com/Lizaakishina/how-to-learn" className="link portfolio__webSite" target="blank">
+            <h3 className="portfolio__webSite-title">Статичный сайт</h3>
+            <div className="portfolio__webSite-arrow">↗</div>
+          </a>
+        </li>
+        <li className="portfolio__line"></li>
+        <li>
+          <a href="https://github.com/Lizaakishina/russian-travel" className="link portfolio__webSite" target="blank">
+            <h3 className="portfolio__webSite-title">Адаптивный сайт</h3>
+            <div className="portfolio__webSite-arrow">↗</div>
+          </a>
+        </li>
+        <li className="portfolio__line"></li>
+        <li>
+          <a href="https://github.com/Lizaakishina/react-mesto-api-full" className="link portfolio__webSite" target="blank">
+            <h3 className="portfolio__webSite-title">Одностраничное приложение</h3>
+            <div className="portfolio__webSite-arrow">↗</div>
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
+
+export default Portfolio;
