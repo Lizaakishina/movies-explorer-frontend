15213b4a3d33cf1224db0a88ff8c88916b65bca6
LizaAkishina <lizaakishina@gmail.com>
2022-11-22 19:00:47 +0300

    feat: вертска технологий компонента techs

--- src/components/Main/Techs/Techs.jsx
+++ src/components/Main/Techs/Techs.jsx
@@ -7,6 +7,31 @@ const Techs = () => {
       <Title title = "Технологии" />
       <h3 className="techs__title">7 технологий</h3>
       <p className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <div className="techs__flex-container">
+        <ul className="list techs__flexbox">
+          <li className="techs__item">
+            <div className="techs__technology">HTML</div>
+          </li>
+          <li className="techs__item">
+            <div className="techs__technology">CSS</div>
+          </li>
+          <li className="techs__item">
+            <div className="techs__technology">JS</div>
+          </li>
+          <li className="techs__item">
+            <div className="techs__technology">React</div>
+          </li>
+          <li className="techs__item">
+            <div className="techs__technology">Git</div>
+          </li>
+          <li className="techs__item">
+            <div className="techs__technology">Express.js</div>
+          </li>
+          <li className="techs__item">
+            <div className="techs__technology">mongoDB</div>
+          </li>
+        </ul>
+      </div>
     </section>
   )
 }
