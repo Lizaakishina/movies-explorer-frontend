8843c28e867f0fb9ea93e9b40b868d5a3a4dc80e
LizaAkishina <lizaakishina@gmail.com>
2022-11-22 17:11:14 +0300

    feat: вертска промо компонента

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,13 @@
 import Header from '../Header/Header';
+import Promo from './Promo/Promo';
 
 const Main = () => {
     return (
       <>
         <Header logIn={false}/>
+        <main>
+          <Promo />
+        </main>
       </>
     )
   }
