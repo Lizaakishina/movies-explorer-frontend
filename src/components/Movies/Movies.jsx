ca726b83398c3e8c3e7542eb6faf8bbc18f3fb6f
LizaAkishina <lizaakishina@gmail.com>
2022-11-23 10:24:05 +0300

    feat: добавление футера на страницу фильмов

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,11 @@
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Movies = () => {
     return (
       <>
         <Header logIn={true} />
+        <Footer />
       </>
     )
   }
