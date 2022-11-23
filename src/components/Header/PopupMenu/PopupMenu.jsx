528cd6a9e1cc333370cb7d2a16ce381034d630fe
LizaAkishina <lizaakishina@gmail.com>
2022-11-23 22:34:18 +0300

    feat: открытие/закрытие попапа меню

--- src/components/Header/PopupMenu/PopupMenu.jsx
+++ src/components/Header/PopupMenu/PopupMenu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import line from "../../../images/line.svg"
+import line from "../../../images/line.svg";
+import closeButton from "../../../images/close-button.svg";
 import useOpenPopup from "../../../hook/useOpenPopup";
 import "./PopupMenu.css";
 
 const PopupMenu = ({logIn}) => {
-  const {handlePopupClick} = useOpenPopup(false);
+  const {handlePopupClick, isButtonPopup} = useOpenPopup(false);
   return (
     <section className="popupMenu">
       <button type="button" className={`button popupMenu__button ${logIn && "popupMenu__button_type_logIn"}`} onClick={handlePopupClick}>
@@ -12,6 +13,11 @@ const PopupMenu = ({logIn}) => {
         <img className="popupMenu__line" src={line} alt="Линия"/>
         <img className="popupMenu__line" src={line} alt="Линия"/>
       </button>
+      <div className={`popupMenu__container ${isButtonPopup && "popupMenu__container_opened"}`}>
+        <button type="button" className="button popupMenu__button popupMenu__button_type_close" onClick={handlePopupClick}>
+          <img className="popupMenu__close" src={closeButton} alt="Крестик"/>
+        </button>
+      </div>
     </section>
   )
 }
