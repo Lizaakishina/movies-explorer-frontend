5280635ee1dfb5be6def24b1a5ed40d15f941ddd
LizaAkishina <lizaakishina@gmail.com>
2022-11-23 23:06:38 +0300

    feat: наполнение popupMenu

--- src/components/Header/PopupMenu/PopupMenu.jsx
+++ src/components/Header/PopupMenu/PopupMenu.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import line from "../../../images/line.svg";
+import chel from "../../../images/chel.svg";
 import closeButton from "../../../images/close-button.svg";
 import useOpenPopup from "../../../hook/useOpenPopup";
 import "./PopupMenu.css";
@@ -17,6 +19,17 @@ const PopupMenu = ({logIn}) => {
         <button type="button" className="button popupMenu__button popupMenu__button_type_close" onClick={handlePopupClick}>
           <img className="popupMenu__close" src={closeButton} alt="Крестик"/>
         </button>
+        <div className="popupMenu__flexbox">
+          <NavLink exact to="/" className="link popupMenu__link" activeClassName="popupMenu__link_active">Главная</NavLink>
+          <NavLink to="/movies" className="link popupMenu__link" activeClassName="popupMenu__link_active">Фильмы</NavLink>
+          <NavLink to="/saved-movies" className="link popupMenu__link" activeClassName="popupMenu__link_active">Сохранённые фильмы</NavLink>
+        </div>
+        <div className="popupMenu__navigation">
+          <NavLink to="/profile" className="link popupMenu__link_type_acc">Аккаунт</NavLink>
+          <div className="popupMenu__chelbox">
+            <img src={chel} className="popupMenu__chel" alt='человечек'/>
+          </div>
+        </div>
       </div>
     </section>
   )
