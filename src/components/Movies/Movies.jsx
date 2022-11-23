1b0aaf42fa09e4209a34c7f37b677627f628b7a1
LizaAkishina <lizaakishina@gmail.com>
2022-11-23 11:03:42 +0300

    feat: верстка формы поиска фильмов

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,14 @@
 import Header from "../Header/Header";
+import SearchForm from "./SearchForm/SearchForm";
 import Footer from "../Footer/Footer";
 
 const Movies = () => {
     return (
       <>
         <Header logIn={true} />
+        <main>
+          <SearchForm />
+        </main>
         <Footer />
       </>
     )
