90d524f3ddb830c1c645c253dcc92a44cf712ae2
LizaAkishina <lizaakishina@gmail.com>
2022-11-29 16:47:01 +0300

    fix: корректное отображение названия и длительности фильма

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,19 +4,19 @@ import close from "../../images/close.svg";
 import save_disactive from "../../images/save_disactive.svg";
 import { useLocation } from "react-router";
 
-const MovieCard = ({nameRus, image, time, trailer}) => {
+const MovieCard = ({movie, image, trailer}) => {
   const url = useLocation();
   const isSaved = true;
   const moviesCardSaved = isSaved ? save_active : save_disactive;
   const moviesCardClose = url.pathname==="/movies" ? moviesCardSaved : close;
-  const hour = (time / 60).toFixed(0);
-  const minute = time % 60;
+  const hour = (movie.duration / 60).toFixed(0);
+  const minute = movie.duration % 60;
   
   return (
     <div className="movieCard">
       <div className="movieCard__flexbox">
         <div className="movieCard__description">
-          <h3 className="movieCard__name">{nameRus}</h3>
+          <h3 className="movieCard__name">{movie.nameRU}</h3>
           <p className="movieCard__time">{hour}ч {minute < 10 ? "0" + minute : minute}м</p>
         </div>
         <button type="button" className="button">
