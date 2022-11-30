59e43ab42a95eb058ca09832da3cf158e067705d
LizaAkishina <lizaakishina@gmail.com>
2022-11-30 10:49:08 +0300

    feat: валидация страниц логирования и регистрации

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,17 +2,40 @@ import {Link} from "react-router-dom";
 import Fieldset from "../Fieldset/Fieldset";
 import logo from "../../images/logo.svg";
 import "./Login.css";
+import { useValidation } from "../../hook/useValidation";
 
 const Login = () => {
+  const { handleChange, errors, isValid } = useValidation();
+
   return (
     <main>
       <section className="login">
         <img className="login__logo" src={logo} alt="Логотип"/>
         <h2 className="login__hello">Рады видеть!</h2>
         <form className="login__form form" noValidate>
-          <Fieldset input = "email" inputType = "email" placeholder = "E-mail"/>
-          <Fieldset input = "password" inputType = "password" placeholder = "Пароль"/>
-          <button className="button form__button">Войти</button> 
+          <Fieldset
+            input = "email"
+            inputType = "email"
+            placeholder = "E-mail"
+            name="email"
+            minLength="4"
+            maxLength="40"
+            onChange={handleChange}
+            errors={errors}
+            isValid={isValid}
+          />
+          <Fieldset
+            input = "password"
+            inputType = "password"
+            placeholder = "Пароль"
+            name="password"
+            minLength="8"
+            maxLength="50"
+            onChange={handleChange}
+            errors={errors}
+            isValid={isValid}
+          />
+          <button className={`button form__button ${!isValid && "form__button_inactive"}`} disabled={!isValid}>Войти</button> 
         </form>
         <p className="login__ask">Ещё не зарегистрированы? <Link to="/signup" className="link login__link">Регистрация</Link></p>
       </section>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,18 +2,52 @@ import {Link} from "react-router-dom";
 import Fieldset from "../Fieldset/Fieldset";
 import logo from "../../images/logo.svg";
 import "./Register.css";
+import { useValidation } from "../../hook/useValidation";
 
 const Register = () => {
+  const { handleChange, errors, isValid } = useValidation();
+
   return (
     <main>
       <section className="register">
         <img className="register__logo" src={logo} alt="Логотип"/>
         <h2 className="register__hello">Добро пожаловать!</h2>
         <form className="form register__form" noValidate>
-          <Fieldset input = "text" inputType = "name" placeholder = "Имя"/>
-          <Fieldset input = "email" inputType = "email" placeholder = "E-mail"/>
-          <Fieldset input = "password" inputType = "password" placeholder = "Пароль" errorMessage="Что-то пошло не так..."/>
-          <button className="button form__button">Зарегистрироваться</button> 
+          <Fieldset
+            input = "text"
+            inputType = "name"
+            placeholder = "Имя"
+            name="name"
+            minLength="4"
+            maxLength="40"
+            onChange={handleChange}
+            errors={errors}
+            isValid={isValid}
+          />
+          <Fieldset
+            input = "email"
+            inputType = "email"
+            placeholder = "E-mail"
+            name="email"
+            minLength="4"
+            maxLength="40"
+            onChange={handleChange}
+            errors={errors}
+            isValid={isValid}
+          />
+          <Fieldset
+            input = "password"
+            inputType = "password"
+            placeholder = "Пароль"
+            errorMessage="Что-то пошло не так..."
+            name="password"
+            minLength="8"
+            maxLength="50"
+            onChange={handleChange}
+            errors={errors}
+            isValid={isValid}
+          />
+          <button className={`button form__button ${!isValid && "form__button_inactive"}`} disabled={!isValid}>Зарегистрироваться</button>
         </form>
         <p className="register__question">Уже зарегистрированы? <Link to="/signin" className="link register__link">Войти</Link></p>
       </section>
