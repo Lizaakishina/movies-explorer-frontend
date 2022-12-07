7ea4d9b60a311fab95bd51cd96d901f0aab422ed
LizaAkishina <lizaakishina@gmail.com>
2022-12-07 21:01:09 +0300

    fix: отображение сообщение об ошибке в профиле

--- src/components/ErrorPopup/ErrorPopup.jsx
+++ src/components/ErrorPopup/ErrorPopup.jsx
@@ -1,10 +1,10 @@
 import "./ErrorPopup.css";
 
-const ErrorPopup = ({isOpen, onClose, errorMessage, onCLoseOverlay}) => {
+const ErrorPopup = ({isOpen, onClose, errorMessage, onCLoseOverlay, isError}) => {
   return (
     <section className={`errorPopup ${isOpen && "errorPopup_opened"}`} onMouseDown={onCLoseOverlay}>
       <div className="errorPopup__container">
-        <h2 className="errorPopup__title">Ошибка</h2>
+        <h2 className="errorPopup__title">{isError && "Ошибка"}</h2>
         <p className="errorPopup__subtitle">{errorMessage}</p>
         <button type="button" className="button errorPopup__button" onClick={onClose}>Ок</button>
       </div>
